cd8aa99357d281f5e8ace56b30c6ef90c2ffb1eb
Toralf Richter <73883286+richtertoralf@users.noreply.github.com>
2022-03-28 12:23:16 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 # Download und Start der Datei im Terminal mit:
-# bash -c "$(curl -k -s https://raw.githubusercontent.com/richtertoralf/testonlinesetup/main/update.sh)"
+# bash -c "$(curl -k -s https://raw.githubusercontent.com/richtertoralf/testonlinesetup/main/update.sh?$(date +%s))"
+# der Querystring ?$(date +%s) sorgt dafür, das immer die aktuellste Datei heruntergeladen wird, da Server/Proxys 
+# immer eine neue URL bekommen.
 
 apt update -y && apt upgrade -y
+echo "Update fertig :-)"
